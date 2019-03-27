6baa4a2a83d01e51eaa7b0e0d5deb7dd38634e4a
ajmoriss <ajmoriss444@gmail.com>
2019-03-27 11:06:13 -0700

    MongoDb panel added to debugger toolbar.

--- main.py
+++ main.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 # Debugger Toolbar
 app.debug = True
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['DEBUG_TB_PROFILER_ENABLED'] = True # Enable the profiler on all requests
 toolbar = DebugToolbarExtension(app)
 # MongoDB initialization
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['DEBUG_TB_PANELS'] = ['flask_mongoengine.panels.MongoDebugPanel']
+app.config['DEBUG_TB_PROFILER_ENABLED'] = True # Enable the profiler on all requests
 db = MongoEngine(app)
+
 
 @app.route('/')
 def index():
