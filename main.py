64cee5857dc6a2ba35dae7404f7b10d17e0acc94
ajmoriss <ajmoriss444@gmail.com>
2019-03-27 08:08:04 -0700

    Debugger Toolbar extension installed and enabled.

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
 from flask import Flask, render_template
 from flask_mongoengine import MongoEngine
+from flask_debugtoolbar import DebugToolbarExtension
 
 
 app = Flask(__name__)
+# Debugger Toolbar
+app.debug = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['DEBUG_TB_PROFILER_ENABLED'] = True # Enable the profiler on all requests
+toolbar = DebugToolbarExtension(app)
+# MongoDB initialization
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 db = MongoEngine(app)
 
@@ -13,4 +20,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
