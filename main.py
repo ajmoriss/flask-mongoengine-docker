26c6b3296c251806238e9718ef512cc93ad80447
ajmoriss <ajmoriss444@gmail.com>
2019-03-27 11:57:36 -0700

    Added all available panels to the debugger toolbar

--- main.py
+++ main.py
@@ -11,8 +11,19 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 toolbar = DebugToolbarExtension(app)
 # MongoDB initialization
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
-app.config['DEBUG_TB_PANELS'] = ['flask_mongoengine.panels.MongoDebugPanel']
+# Available panels at the debugger toolbar
+app.config['DEBUG_TB_PANELS'] = [
+    'flask_debugtoolbar.panels.versions.VersionDebugPanel',
+    'flask_debugtoolbar.panels.timer.TimerDebugPanel',
+    'flask_debugtoolbar.panels.headers.HeaderDebugPanel',
+    'flask_debugtoolbar.panels.request_vars.RequestVarsDebugPanel',
+    'flask_debugtoolbar.panels.template.TemplateDebugPanel',
+    'flask_debugtoolbar.panels.logger.LoggingPanel',
+    'flask_debugtoolbar.panels.profiler.ProfilerDebugPanel',
+    'flask_mongoengine.panels.MongoDebugPanel',
+]
 app.config['DEBUG_TB_PROFILER_ENABLED'] = True # Enable the profiler on all requests
+
 db = MongoEngine(app)
 
 
